3aa0b00296ec331b510d03043338dd570e0c7088
mariaclarasslv <sousasilvamariaclara16@gmail.com>
2024-03-07 10:48:12 -0300

    Rota

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Title from './src/components/';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Maria Clara de Sousa Silva</Text>
       <Text>Desenvolvimento de Dispositivos Móveis</Text>
+      <Title/>
       <StatusBar style="auto" />
     </View>
   );
