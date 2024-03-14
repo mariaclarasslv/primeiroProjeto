59c677d2b1cd33671f045d61bee17782829b7117
mariaclarasslv <sousasilvamariaclara16@gmail.com>
2024-03-14 10:34:18 -0300

    novo botão

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Title from './src/components/';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Maria Clara de Sousa Silva</Text>
       <Text>Desenvolvimento de Dispositivos Móveis</Text>
-      <Title/>
+      <Button title='salvar'></Button>
+   
       <StatusBar style="auto" />
     </View>
   );
